e10f2de50add57baa93ab6ec217e879e4dcb55a7
berry <matthewberryhill@gmail.com>
2017-08-30 20:02:59 -0600

    Implement pagination on endpoint GET/products/:product

--- api/server/products.go
+++ api/server/products.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"strconv"
 	"net/http"
 
 	"github.com/berryhill/gf-api/api/models"
@@ -12,10 +13,24 @@ func GetProducts(c echo.Context) error {
 
 	page := 1
 	per_page := 20
+	for key, value := range c.QueryParams() {
+		if key == "page" {
+			page, _ = strconv.Atoi(value[0])
+			delete(c.QueryParams(), "page")
+ 		} else if key == "per-page" {
+			per_page, _ = strconv.Atoi(value[0])
+			delete(c.QueryParams(), "per-page")
+		}
+	}
 
 	product := c.Param("product")
 
-	products, _ := models.GetProducts(product, c.QueryParams())
+	products, _ := models.GetProducts(product, c.QueryParams(), page, per_page)
+	var products_paginated []*models.Product
+	for k := 0; k < per_page; k++ {
+		products_paginated = append(
+			products_paginated, (products[(((k+1)*page)-1)]))
+	}
 
 	metadata := make(map[string]interface{})
 	metadata["page"] = page
@@ -24,7 +39,7 @@ func GetProducts(c echo.Context) error {
 
 	response := make(map[string]interface{})
 	response["metadata"] = metadata
-	response["results"] = products
+	response["results"] = products_paginated
 
 	return c.JSON(http.StatusOK, response)
 }
